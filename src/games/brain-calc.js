7d615638d40cadc506ed26a3bf2c46230e599fab
kelevra-pro <damtat8@gmail.com>
2021-04-25 23:33:31 +0300

    feat: add game engine

--- src/games/brain-calc.js
+++ src/games/brain-calc.js
@@ -1,17 +1,8 @@
-import readlineSync from 'readline-sync';
-import greeting from '../cli.js';
+import _ from 'lodash';
+import gameEngine from '../index.js';
 
-const getRandomInt = (minimum, maximum) => {
-  const min = Math.ceil(minimum);
-  const max = Math.floor(maximum);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-};
-
-const getRandomOperation = () => {
-  const index = getRandomInt(0, 2);
-  const operations = ['+', '-', '*'];
-  return operations[index];
-};
+const rule = 'What is the result of the expression?';
+const operations = ['+', '-', '*'];
 
 const getRightAnswer = (num1, num2, operation) => {
   switch (operation) {
@@ -26,34 +17,18 @@ const getRightAnswer = (num1, num2, operation) => {
   }
 };
 
-const calcGame = () => {
-  const playerName = greeting();
+const gameData = () => {
+  const num1 = _.random(0, 49);
+  const num2 = _.random(0, 49);
+  const randomIndex = _.random(0, operations.length - 1);
+  const operation = operations[randomIndex];
 
-  console.log('What is the result of the expression?');
+  const task = `${num1} ${operation} ${num2}`;
+  const rightAnswer = getRightAnswer(num1, num2, operation);
 
-  for (let i = 3; i > 0; i -= 1) {
-    const num1 = getRandomInt(1, 49);
-    const num2 = getRandomInt(1, 49);
-    const operation = getRandomOperation();
-    const rightAnswer = getRightAnswer(num1, num2, operation);
-
-    console.log(`Question: ${num1} ${operation} ${num2}`);
-
-    const answer = readlineSync.question('Your answer: ');
-
-    if (rightAnswer === Number(answer)) {
-      console.log('Correct!');
-      if (i === 1) {
-        console.log(`Congratulations, ${playerName}!`);
-      }
-    } else {
-      console.log(
-        `'${answer}' is wrong answer ;(. Correct answer was '${rightAnswer}'. \n`,
-        `Let's try again, ${playerName}!`,
-      );
-      return;
-    }
-  }
+  return [task, rightAnswer.toString()];
 };
+
+const calcGame = () => gameEngine(rule, gameData);
 
 export default calcGame;
--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -1,40 +1,17 @@
-import readlineSync from 'readline-sync';
-import greeting from '../cli.js';
+import _ from 'lodash';
+import gameEngine from '../index.js';
 
-const getRandomInt = () => {
-  const min = 1;
-  const max = 99;
-  return Math.floor(Math.random() * (max - min)) + min;
+const rule = 'Answer "yes" if the number is even, otherwise answer "no".';
+
+const getRightAnswer = (num) => (num % 2 === 0 ? 'yes' : 'no');
+
+const gameData = () => {
+  const task = _.random(0, 99);
+  const rightAnswer = getRightAnswer(task);
+
+  return [task, rightAnswer];
 };
 
-const getRightAnswer = (int) => (int % 2 === 0 ? 'yes' : 'no');
-
-const evenGame = () => {
-  const playerName = greeting();
-
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
-
-  for (let i = 3; i > 0; i -= 1) {
-    const randomInt = getRandomInt();
-    const rightAnswer = getRightAnswer(randomInt);
-
-    console.log(`Question: ${randomInt}`);
-
-    const answer = readlineSync.question('Your answer: ');
-
-    if (rightAnswer === answer) {
-      console.log('Correct!');
-      if (i === 1) {
-        console.log(`Congratulations, ${playerName}!`);
-      }
-    } else {
-      console.log(
-        `'${answer}' is wrong answer ;(. Correct answer was '${rightAnswer}'. \n`,
-        `Let's try again, ${playerName}!`,
-      );
-      return;
-    }
-  }
-};
+const evenGame = () => gameEngine(rule, gameData);
 
 export default evenGame;
